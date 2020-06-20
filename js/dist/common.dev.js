0fa7fe66f29dd836810e1f3eac918e486032f05b
Udovik Aleksey <udovik.aleksey@mail.ru>
2020-06-20 16:49:07 +0300

    Закончена верстка, добавлены стили

--- js/dist/common.dev.js
+++ js/dist/common.dev.js
@@ -4,5 +4,35 @@ $(document).ready(function () {
   $('.nav__switch').click(function () {
     $('.nav__switch').toggleClass('active');
     $('.nav__toggle').toggleClass('active__menu');
-  });
+  }); // $('.nav__switch').click(function(){ 
+  //   $('.nav__switch').addClass('active');
+  //   $('.nav__toggle').addClass('active__menu');
+  //   document.querySelector('.nav__toggle').style['display'] = 'none';
+  //   $('.active').removeClass('nav__switch');
+  //   $('.active__menu').removeClass('nav__toggle');
+  //   document.querySelector('.active__menu').style['display'] = 'flex';
+  // });
+  // $('.active').click(function(){ 
+  //   $('.active').addClass('nav__switch');
+  //   $('.active__menu').addClass('nav__toggle');
+  //   document.querySelector('.active__menu').style['display'] = 'none';
+  //   $('.nav__switch').removeClass('active');
+  //   $('.nav__toggle').removeClass('active__menu');      
+  //   document.querySelector('.nav__toggle').style['display'] = 'flex';
+  // });
+  // $('.slider__for').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   asNavFor: '.slider__nav'
+  // });
+  // $('.slider__nav').slick({
+  //   slidesToShow: 2,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.slider__for',
+  //   dots: true,
+  //   centerMode: true,
+  //   focusOnSelect: true
+  // });
 });
